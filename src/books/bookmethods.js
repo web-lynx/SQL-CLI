8efce3d90a8cb8a60810213868de7799c5dc4000
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-02-10 18:20:52 +0000

    CRUD is go!

--- src/books/bookmethods.js
+++ src/books/bookmethods.js
@@ -21,9 +21,9 @@ exports.listBooks = async () => {
   }
 };
 
-exports.searchBooks = async (bookObject) => {
+exports.searchBooks = async (argv) => {
   try {
-    searchCriteria = Object.entries(bookObject);
+    searchCriteria = Object.entries(argv);
     searchValue = [];
     searchValue.push(searchCriteria[2]);
     searchObj = Object.fromEntries(searchValue);
@@ -34,16 +34,28 @@ exports.searchBooks = async (bookObject) => {
   }
 };
 
+//Selects row using the title, and then changes based on user input
 exports.updateBook = async (argv) => {
   try {
-
+    updateCriteria = Object.entries(argv);
+    updateValue = [];
+    updateValue.push(updateCriteria[3]);
+    updateObj = Object.fromEntries(updateValue);
+    console.log(updateCriteria);
+    await Book.update(updateObj, { where: { title: argv.title } });
   } catch (error) {
     console.log(error);
   }
 };
 
+//Selects row using the title, and then deletes based on user input
 exports.deleteBook = async (argv) => {
   try {
+    deleteCriteria = Object.entries(argv);
+    deleteValue = [];
+    deleteValue.push(deleteCriteria[2]);
+    deleteObj = Object.fromEntries(deleteValue);
+    await Book.destroy({ where: deleteObj });
   } catch (error) {
     console.log(error);
   }
